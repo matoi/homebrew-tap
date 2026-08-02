e35376f9c5520d73e3cb56f2fdbbf3722aa4f673
Noriaki Matoi <noriaki@matoi.org>
2026-08-02 20:26:27 +0900

    Update view-file to 0.1.1

--- Formula/view-file.rb
+++ Formula/view-file.rb
@@ -2,8 +2,8 @@ class ViewFile < Formula
   desc "Format-aware terminal and macOS file viewer"
   homepage "https://github.com/matoi/view-file"
   url "https://github.com/matoi/view-file.git",
-      tag:      "v0.1.0",
-      revision: "8decf817f6459aa5292f9be09f33b3d02805f5ea"
+      tag:      "v0.1.1",
+      revision: "5defa45add0130cc5b1d6dbc993d4171167a31ea"
   license "GPL-3.0-or-later"
   head "https://github.com/matoi/view-file.git", branch: "main"
 
@@ -18,10 +18,12 @@ class ViewFile < Formula
 
   def install
     bin.install "bin/view-file"
+    bin.install_symlink "view-file" => "vf"
   end
 
   test do
     assert_match "usage: view-file", shell_output("#{bin}/view-file --help 2>&1")
-    assert_match "hello", pipe_output("#{bin}/view-file --terminal --type source -", "hello\n")
+    assert_match "usage: view-file", shell_output("#{bin}/vf --help 2>&1")
+    assert_match "hello", pipe_output("#{bin}/view-file --type source -", "hello\n")
   end
 end
